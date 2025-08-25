d7f57aabdefe1ecc3d7b08b226e017cbdaca9138
jemin hwangbo <jemin.hwangbo@gmail.com>
2025-08-26 05:04:17 +0900

    fix:fixed install script

--- templates/install_dependencies.sh
+++ templates/install_dependencies.sh
@@ -17,7 +17,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 shopt -s nullglob
 
 # Iterate over each *direct* sub-directory
-for subdir in "$SCRIPT_DIR/dependencies"/*/; do
+for subdir in "$SCRIPT_DIR/../src"/*/; do
   installer="${subdir}install_dependencies.sh"
 
   # If the installer script exists, run it
