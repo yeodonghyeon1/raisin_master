5e0ea5d3cf133cabe6f185b02b23d506c0b6d1be
jemin <jhwangbo@kaist.ac.kr>
2025-09-05 06:52:09 +0900

    feat: windows compatibility wip

--- cmake/raisin_helper.cmake
+++ cmake/raisin_helper.cmake
@@ -80,7 +80,7 @@ function(raisin_recommended_clang_tidy)
     find_program(CLANG_TIDY_EXE
             NAMES clang-tidy clang-tidy-18 clang-tidy-17 clang-tidy-16 clang-tidy-15 clang-tidy-14)
 
-    if(NOT CLANG_TIDY_EXE AND NOT WIN32)
+    if(CLANG_TIDY_EXE OR WIN32)
         message(WARNING "clang-tidy not found – disabled")
         return()
     endif()
